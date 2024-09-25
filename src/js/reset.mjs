b2ac44d85f5d635ae3b619cdea191413f3b1a0ed
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-09-25 22:20:17 +0200

    Lint test

--- src/js/reset.mjs
+++ src/js/reset.mjs
@@ -1,26 +1,25 @@
-import renderPage from "./render/renderPage.mjs";
-import runApi from "./api/runApi.mjs";
-import resetDisplay from "./resetDisplay.mjs";
+import renderPage from './render/renderPage.mjs';
+import runApi from './api/runApi.mjs';
+import resetDisplay from './resetDisplay.mjs';
 
 export default async function reset() {
-  const resetButton = document.getElementById("retry");
+  const resetButton = document.getElementById('retry');
   const nextPokemon = await runApi();
-  console.log("Next Pokemon", nextPokemon);
+  console.log('Next Pokemon', nextPokemon);
 
-  resetButton.addEventListener("click", function () {
-    const choice1 = document.getElementById("choice1");
-    const choice2 = document.getElementById("choice2");
-    const typings = document.getElementById("typings");
+  resetButton.addEventListener('click', function () {
+    const choice1 = document.getElementById('choice1');
+    const choice2 = document.getElementById('choice2');
+    const typings = document.getElementById('typings');
 
-    if (!choice2)
-      typings.appendChild(choice1.firstChild);
+    if (!choice2) typings.appendChild(choice1.firstChild);
     else {
       typings.appendChild(choice1.firstChild);
       typings.appendChild(choice2.firstChild);
     }
 
     resetDisplay();
-    sessionStorage.setItem("counter", 0);
+    sessionStorage.setItem('counter', 0);
     renderPage(nextPokemon);
   });
 }
