1d0fb133521f24c774a06c7f7e1209d0ac002199
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-10-04 19:02:42 +0200

    Updated function to follow the last commit

--- src/js/stats/updateStats.mjs
+++ src/js/stats/updateStats.mjs
@@ -1,4 +1,6 @@
-export default function updateStats(stats) {
+export default function updateStats() {
+  const stats = JSON.parse(sessionStorage.getItem('stats'));
+
   const correct = document.getElementById('stats-correct');
   const total = document.getElementById('stats-total');
   const percentage = document.getElementById('stats-percentage');
