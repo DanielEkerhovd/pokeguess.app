5d5187876457de9ac7b3ac9c777090b4bf1a2e4f
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-10-12 15:03:16 +0200

    Added vite to the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 
   {
-    ignores: ["tailwind.config.js"],
+    ignores: [
+      'tailwind.config.js',
+      'vite.config.js',
+      'eslint.config.mjs',
+      'postcss.config.js',
+    ],
   },
 ];
