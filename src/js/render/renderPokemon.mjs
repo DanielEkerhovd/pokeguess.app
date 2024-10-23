bfa3785b32da8a4cce506bd34ed8564b35eb0345
Daniel Ekerhovd <115421350+DanielEkerhovd@users.noreply.github.com>
2024-10-23 20:44:34 +0200

    Added function to change all - with spaces

--- src/js/render/renderPokemon.mjs
+++ src/js/render/renderPokemon.mjs
@@ -2,7 +2,9 @@ export default function renderPokemon(pokemon) {
   const imageContainer = document.getElementById('pokemonImage');
   const nameContainer = document.getElementById('pokeName');
 
-  const pokeName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const pokeNameUppercase =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const pokeName = pokeNameUppercase.replace(/-/g, ' ');
   nameContainer.textContent = pokeName;
 
   imageContainer.src = pokemon.image;
